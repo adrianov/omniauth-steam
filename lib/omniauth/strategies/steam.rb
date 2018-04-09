6eed3f97365dea8ecbebbab79760f0024b6db2ab
Peter M Adrianov <peter.adrianov@gmail.com>
2018-04-09 13:14:02 +0300

    Fix auth.

--- lib/omniauth/strategies/steam.rb
+++ lib/omniauth/strategies/steam.rb
@@ -8,7 +8,7 @@ module OmniAuth
 
       option :api_key, nil
       option :name, "steam"
-      option :identifier, "http://steamcommunity.com/openid"
+      option :identifier, "https://steamcommunity.com/openid"
 
       uid { steam_id }
 
@@ -52,7 +52,7 @@ module OmniAuth
       def steam_id
         @steam_id ||= begin
                         claimed_id = openid_response.display_identifier.split('/').last
-                        expected_uri = "http://steamcommunity.com/openid/id/#{claimed_id}"
+                        expected_uri = "https://steamcommunity.com/openid/id/#{claimed_id}"
                         if openid_response.endpoint.claimed_id != expected_uri
                           raise 'Steam Claimed ID mismatch!'
                         end
